adfd159e65b82a7f7cb97eaef5b35d8313242d3f
wxy65011 <18406586927@163.com>
2023-04-04 10:40:41 +0800

    Initial commit

---
+++ .idea/A.js
@@ -0,0 +1,14 @@
+public class A {
+    int m;
+    int n;
+    int c;
+    public A(int m, int n){
+    this.m= m;
+    this.n= n;
+
+}
+public void B(){
+    c=m+n;
+    System.out.println(c);
+}
+}
---
+++ src/com/company/A.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class A {
+    int m;
+    int n;
+    int c;
+    public A(int m, int n){
+        this.m= m;
+        this.n= n;
+
+    }
+    public void B(){
+        c=m+n;
+        System.out.println(c);
+    }
+}
+
