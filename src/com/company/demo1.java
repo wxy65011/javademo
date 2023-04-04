30d0fd54f4039a560cd0b36751d2a8bc186983c4
wxy65011 <129728285+wxy65011@users.noreply.github.com>
2023-04-04 10:41:50 +0800

    Update demo1.java

--- src/com/company/demo1.java
+++ src/com/company/demo1.java
@@ -3,16 +3,4 @@ public class demo1{
 
 public static void main(String[]args){
 
-        int m;
-        int n;
-        int c;
-demo1 add = new demo1();
-    demo1 add2 = add;
-    demo1 add1 = add;
-    public void A(){
-        c=m+n;
-        System.out.println(c);
-        }
-        }
-        }
-
+}
